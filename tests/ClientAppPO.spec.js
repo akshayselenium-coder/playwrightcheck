4207972fd9c0c99a0f549f4099d4b364b14c96d9
Akshay Kumar <akshay1379@gmail.com>
2026-09-24 20:52:59 +0530

    issue resolved

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -89,9 +89,9 @@ test.only('playwright test for single user', async({page}) =>
 {
   const data1=dataset[1];
      const poManager=new POManager(page);
-    const loginPage = await poManager.getLoginPageObject();
+    const loginPage =  poManager.getLoginPageObject();
     await loginPage.goTo();
-   await loginPage.validLogin(data1.userName, data2.password);
+   await loginPage.validLogin(data1.userName, data1.password);
 
 
 });
