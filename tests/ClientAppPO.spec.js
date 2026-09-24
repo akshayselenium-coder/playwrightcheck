96e035d451a4cd6294f1443082b4ccf40156e25a
Akshay Kumar <akshay1379@gmail.com>
2026-09-24 18:17:10 +0530

    data3

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -91,7 +91,7 @@ test.only('playwright test for single user', async({page}) =>
      const poManager=new POManager(page);
     const loginPage = await poManager.getLoginPageObject();
     await loginPage.goTo();
-   await loginPage.validLogin(data1.userName, data2.passord);
+   await loginPage.validLogin(data1.userName, data3.passord);
 
 
 });
